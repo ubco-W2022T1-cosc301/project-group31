647f586d6580a73a991b43b5d723beedf4723517
LLUISEM2002 <lluis@student.ubc.ca>
2022-12-02 18:10:18 -0800

    updated function

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -16,16 +16,17 @@ def load_and_process(data):
     """
 
     df = data.copy()
-    # transform InvoiceDate into date time format
+
     df['InvoiceDate'] = pd.to_datetime(df.InvoiceDate, format='%m/%d/%Y %H:%M')
 
-    # create column Total cost of order
+
     df["TotalCost"] = df["Quantity"] * df["UnitPrice"]
-    """
+
+
     df.insert(loc=2, column='year_month', value=df['InvoiceDate'].map(lambda x: 100*x.year + x.month))
     df.insert(loc=3, column='month', value=df.InvoiceDate.dt.month)
-    # +1 to make Monday=1.....until Sunday=7
+
     df.insert(loc=4, column='day', value=(df.InvoiceDate.dt.dayofweek)+1)
     df.insert(loc=5, column='hour', value=df.InvoiceDate.dt.hour)
-    """
-    return df
+
+    return data
