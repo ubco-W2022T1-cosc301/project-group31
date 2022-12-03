4f1ec0ad2b0d29657735d9776691e62119a26655
LLUISEM2002 <lluis@student.ubc.ca>
2022-12-02 18:29:52 -0800

    updated function

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def load_and_process(data):
     """
-    Adding new columns to the data set for ease of use in analysis
+    Adding new columns to the data set for ease of use in analysis and convert date to appropriate format
 
         Parameters
     ----------
@@ -19,16 +19,6 @@ def load_and_process(data):
 
     df['InvoiceDate'] = pd.to_datetime(df.InvoiceDate, format='%m/%d/%Y %H:%M')
 
-
     df["TotalCost"] = df["Quantity"] * df["UnitPrice"]
-
-    """
-    df.insert(loc=2, column='year_month', value=df['InvoiceDate'].map(lambda x: 100*x.year + x.month))
-    df.insert(loc=3, column='month', value=df.InvoiceDate.dt.month)
-
-    df.insert(loc=4, column='day', value=(df.InvoiceDate.dt.dayofweek)+1)
-    df.insert(loc=5, column='hour', value=df.InvoiceDate.dt.hour)
-    """
-    df["month"] = df["InvoiceDate"]
 
     return df
