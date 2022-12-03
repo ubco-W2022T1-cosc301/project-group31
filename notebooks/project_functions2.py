2c9e01bc9d190e3b376a6164df1358d4bbee8737
LLUISEM2002 <lluis@student.ubc.ca>
2022-12-02 18:16:35 -0800

    updated function

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -22,11 +22,13 @@ def load_and_process(data):
 
     df["TotalCost"] = df["Quantity"] * df["UnitPrice"]
 
-
+    """
     df.insert(loc=2, column='year_month', value=df['InvoiceDate'].map(lambda x: 100*x.year + x.month))
     df.insert(loc=3, column='month', value=df.InvoiceDate.dt.month)
 
     df.insert(loc=4, column='day', value=(df.InvoiceDate.dt.dayofweek)+1)
     df.insert(loc=5, column='hour', value=df.InvoiceDate.dt.hour)
+    """
+    df["month"] = df["InvoiceDate"]
 
-    return data
+    return df
